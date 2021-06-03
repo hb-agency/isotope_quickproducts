de6c8a1d3151efa3ea016f03bd3e4b75af09c335
Ingolf Steinhardt <info@e-spin.de>
2021-06-03 17:38:41 +0200

    Add manual sorting for selected products

--- src/Resources/contao/languages/de/tl_content.php
+++ src/Resources/contao/languages/de/tl_content.php
@@ -13,35 +13,38 @@
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_content']['iso_products'] = array(
+$GLOBALS['TL_LANG']['tl_content']['iso_products']             = [
     'Welche Produkte sollen angezeigt werden?',
     'Wählen Sie die Produkte aus, welche als Liste angezeigt werden sollen.'
-);
-$GLOBALS['TL_LANG']['tl_content']['iso_gallery'] = array('Galerie', 'Wählen Sie eine Galerie, um die Bilder zu rendern (Überschreibt die Produkttyp-Einstellung).');
-$GLOBALS['TL_LANG']['tl_content']['iso_list_layout'] = array(
+];
+$GLOBALS['TL_LANG']['tl_content']['iso_products_searchLabel'] = 'Produktsuche';
+$GLOBALS['TL_LANG']['tl_content']['iso_gallery']              =
+    ['Galerie', 'Wählen Sie eine Galerie, um die Bilder zu rendern (Überschreibt die Produkttyp-Einstellung).'];
+$GLOBALS['TL_LANG']['tl_content']['iso_list_layout']          = [
     'Produktlisten-Template',
     'Bitte wählen Sie ein Listen-Layout.'
-);
-$GLOBALS['TL_LANG']['tl_content']['iso_listingSortField'] = array(
+];
+$GLOBALS['TL_LANG']['tl_content']['iso_listingSortField']     = [
     'Anfängliches Sortierfeld',
-    'Wählen Sie auf Basis welches Sortierfeldes die Liste beim ersten Laden der Seite sortiert werden soll.'
-);
-$GLOBALS['TL_LANG']['tl_content']['iso_listingSortDirection'] = array(
+    'Wählen Sie auf Basis welches Sortierfeldes die Liste beim ersten Laden der Seite sortiert werden soll. Wenn nichts ausgewählt ist, wird die manuelle Sortierung übernommen.'
+];
+$GLOBALS['TL_LANG']['tl_content']['iso_listingSortDirection'] = [
     'Anfängliche Sortierrichtung',
     'Wählen Sie eine anfängliche Sortierrichtung.'
-);
-$GLOBALS['TL_LANG']['tl_content']['iso_buttons'] = array('Schaltflächen', 'Wählen Sie welche Schaltflächen angezeigt werden sollen.');
-$GLOBALS['TL_LANG']['tl_content']['iso_cols'] = array(
+];
+$GLOBALS['TL_LANG']['tl_content']['iso_buttons']              =
+    ['Schaltflächen', 'Wählen Sie welche Schaltflächen angezeigt werden sollen.'];
+$GLOBALS['TL_LANG']['tl_content']['iso_cols']                 = [
     'Spalten',
     'Geben sie an wieviele Spalten der Breite nach im Listen-Template angezeigt werden sollen.'
-);
-$GLOBALS['TL_LANG']['tl_content']['iso_use_quantity'] = array(
+];
+$GLOBALS['TL_LANG']['tl_content']['iso_use_quantity']         = [
     'Menge aktivieren',
     'Ermöglicht es den Nutzern anzugeben, dass 1 oder mehrere Stück eines Produkts gekauft werden sollen.'
-);
+];
 
 /**
  * Legends
  */
-$GLOBALS['TL_LANG']['tl_content']['config_legend'] = 'Configuration settings';
+$GLOBALS['TL_LANG']['tl_content']['config_legend']   = 'Configuration settings';
 $GLOBALS['TL_LANG']['tl_content']['template_legend'] = 'Template-Einstellungen';
--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -13,14 +13,22 @@
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_content']['iso_products']       = array('Choose products to display', 'Choose the products to display in the list format.');
-$GLOBALS['TL_LANG']['tl_content']['iso_gallery']        = array('Choose a gallery', 'Choose the image gallery to display.');
-$GLOBALS['TL_LANG']['tl_content']['iso_list_layout']    = array('List template', 'Choose the template to use with the products.');
-$GLOBALS['TL_LANG']['tl_content']['iso_listingSortField']            = array('Initial sorting field', 'Select a sorting field to sort the listing by on first page load.');
-$GLOBALS['TL_LANG']['tl_content']['iso_listingSortDirection']        = array('Initial sorting direction', 'Select a initial sorting direction.');
-$GLOBALS['TL_LANG']['tl_content']['iso_buttons']                     = array('Buttons', 'Select the buttons you want to show.');
-$GLOBALS['TL_LANG']['tl_content']['iso_cols']                        = array('Columns', 'Enter a number of columns to display width-wise in the listing template.');
-$GLOBALS['TL_LANG']['tl_content']['iso_use_quantity']                = array('Enable quantity', 'Allow users to specify 1 or more of a product to be purchased.');
+$GLOBALS['TL_LANG']['tl_content']['iso_products']             =
+    ['Choose products to display', 'Choose the products to display in the list format.'];
+$GLOBALS['TL_LANG']['tl_content']['iso_products_searchLabel'] = 'Search products';
+$GLOBALS['TL_LANG']['tl_content']['iso_gallery']              =
+    ['Choose a gallery', 'Choose the image gallery to display.'];
+$GLOBALS['TL_LANG']['tl_content']['iso_list_layout']          =
+    ['List template', 'Choose the template to use with the products.'];
+$GLOBALS['TL_LANG']['tl_content']['iso_listingSortField']     =
+    ['Initial sorting field', 'Select a sorting field to sort the listing by on first page load. If nothing is selected, the manual sorting is used.'];
+$GLOBALS['TL_LANG']['tl_content']['iso_listingSortDirection'] =
+    ['Initial sorting direction', 'Select a initial sorting direction.'];
+$GLOBALS['TL_LANG']['tl_content']['iso_buttons']              = ['Buttons', 'Select the buttons you want to show.'];
+$GLOBALS['TL_LANG']['tl_content']['iso_cols']                 =
+    ['Columns', 'Enter a number of columns to display width-wise in the listing template.'];
+$GLOBALS['TL_LANG']['tl_content']['iso_use_quantity']         =
+    ['Enable quantity', 'Allow users to specify 1 or more of a product to be purchased.'];
 
 /**
  * Legends
