3bc6be05699e1f8f5169ff616782f856e9fc6e29
zonky2 <info@e-spin.de>
2022-07-22 11:10:45 +0200

    Fix checkbox css class

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -71,7 +71,7 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['iso_use_quantity'] = array
     'label'                     => &$GLOBALS['TL_LANG']['tl_content']['iso_use_quantity'],
     'exclude'                   => true,
     'inputType'                 => 'checkbox',
-    'eval'                      => array('tl_class'=>'w50'),
+    'eval'                      => array('tl_class'=>'w50 m12'),
     'sql'                       => "char(1) NOT NULL default ''",
 );
 
